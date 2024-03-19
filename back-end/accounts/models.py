56f9406ee06fdaaed934e3ddc7242653e94e25ac
Mohammad Baharlou <mohammadbaharloo96@gmail.com>
2024-03-20 02:57:17 +0330

    initial model of accounts

--- back-end/accounts/models.py
+++ back-end/accounts/models.py
@@ -1,12 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from .validators import phone_validator
+
+
 class User(AbstractUser):
     class Gender(models.TextChoices):
         MALE = 'M', 'Male'
         FEMALE = 'F', 'Female'
         UNSET = 'MF', 'Unset'
 
+    phone = models.CharField(max_length=15, validators=[phone_validator], blank=True)
     address = models.TextField(blank=True)
     gender = models.CharField(max_length=2, choices=Gender.choices, default=Gender.UNSET)
     age = models.PositiveSmallIntegerField(blank=True, null=True)
