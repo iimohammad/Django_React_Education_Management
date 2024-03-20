056b7d8392b2f9b81ec4e1fe27b5eb53bfa6c244
masih1999-12 <msh77742@gmail.com>
2024-03-21 00:22:42 +0330

    create enrollment models

--- back-end/enrollment/models.py
+++ back-end/enrollment/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class UnitSelectionRequest(models.Model):
+    student = models.ForeignKey('accounts.Student' , on_delete = models.CASCADE)
+    course = models.ForeignKey('education.SemesterCourse' , on_delete = models.CASCADE)
+    status = models.CharField(max_length = 50)
+
+class RevisionRequest(models.Model):
+    student = models.ForeignKey('accounts.Student' , on_delete = models.CASCADE)
+    course = models.ForeignKey('education.SemesterCourse' , on_delete = models.CASCADE)
+    text = models.TextField()
+    answer = models.TextField()
+    
+class AddRemoveRequest(models.Model):
+    student = models.ForeignKey('accounts.Student' , on_delete = models.CASCADE)
+    removed_courses = models.ManyToManyField('education.SemesterCourse' , on_delete = models.CASCADE)
+    added_courses = models.ManyToManyField('education.SemesterCourse' , on_delete = models.CASCADE)
+    status = models.CharField(max_length = 50)
+    
+class EmergencyRemovalRequest(models.Model):
+    student = models.ForeignKey('accounts.Student' , on_delete = models.CASCADE)
+    course = models.ForeignKey('education.SemesterCourse' , on_delete = models.CASCADE)
+    status = models.CharField(max_length = 50)
+    student_explanation = models.TextField()
+    educational_assistant_explanation = models.TextField()
+    
+class EmploymentEducationRequest(models.Model):
+    student = models.ForeignKey('accounts.Student' , on_delete = models.CASCADE)
+    semester = models.ForeignKey('education.Semester' , on_delete = models.CASCADE)
