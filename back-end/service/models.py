dff30df7502b0657b606d0e31cb4cbcbdea95d91
Mohammad Baharlou <mohammadbaharloo96@gmail.com>
2024-03-21 11:45:38 +0330

    update models

--- back-end/service/models.py
+++ back-end/service/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from accounts.models import Student
 # Create your models here.
+class EmploymentEducationRequest(models.Model):
+    student = models.ForeignKey(Student , on_delete = models.CASCADE)
+    semester = models.ForeignKey('education.Semester' , on_delete = models.CASCADE)
+    certificate_issuance_place = models.CharField(max_length = 255)
+    study_employment_file = models.FileField(upload_to='enrollment/study_employment_files/' , null=True , blank=True)
