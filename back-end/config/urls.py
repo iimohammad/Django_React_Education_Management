7e624b4c1878cac8f99bdb1ddade1c7733c8df50
Amirabbas <abb.afrasiyabi@gmail.com>
2024-03-22 14:52:43 +0330

    alredy uptodate

--- back-end/config/urls.py
+++ back-end/config/urls.py
@@ -1,17 +1,11 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from django.urls import path,include
-=======
 from django.urls import path, include
->>>>>>> 2a8ee34cd1303c33b5151441d0aa994874073eab
 from config import local_settings
 
 
 urlpatterns = [
     # Admin Urls
     path(local_settings.Admin, admin.site.urls),
-<<<<<<< HEAD
-=======
 
     # App Urls
     path('accounts/', include('accounts.urls'), name='blog'),
@@ -20,5 +14,4 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api-auth/', include('rest_framework.urls')),
->>>>>>> 2a8ee34cd1303c33b5151441d0aa994874073eab
 ]
