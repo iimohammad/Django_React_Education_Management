260f402c6864a2a5f016edc829b3e1b98e554cad
masih1999-12 <msh77742@gmail.com>
2024-03-25 00:30:37 +0330

    set settings for swagger

--- back-end/config/settings.py
+++ back-end/config/settings.py
@@ -31,7 +31,8 @@ THIRD_PARTY_APPS = [
     'rest_framework.authtoken',
     'django_filters',
     'rest_framework_simplejwt',
-    'rest_framework_swagger',
+    'drf_spectacular',
+    'drf_spectacular_sidecar',
     'graphene_django',
 ]
 
@@ -145,16 +146,29 @@ else:
             'rest_framework.authentication.TokenAuthentication'
         )
     }
+REST_FRAMEWORK['DEFAULT_SCHEMA_CLASS'] = 'drf_spectacular.openapi.AutoSchema'
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Education Management API',
+    'DESCRIPTION': 'a project for managing universicty',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    'SWAGGER_UI_DIST': 'SIDECAR',  
+    'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
+    'REDOC_DIST': 'SIDECAR',
+}
+
+
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-# Email Configurations
-EMAIL_BACKEND = local_settings.Email_Configuration['EMAIL_BACKEND']
-EMAIL_HOST = local_settings.Email_Configuration['EMAIL_HOST']
-EMAIL_PORT = local_settings.Email_Configuration['EMAIL_PORT']
-EMAIL_HOST_USER = local_settings.Email_Configuration['EMAIL_HOST_USER']
-EMAIL_HOST_PASSWORD = local_settings.Email_Configuration['EMAIL_HOST_PASSWORD']
-EMAIL_USE_TLS = local_settings.Email_Configuration['EMAIL_USE_TLS']
+# # Email Configurations
+# EMAIL_BACKEND = local_settings.Email_Configuration['EMAIL_BACKEND']
+# EMAIL_HOST = local_settings.Email_Configuration['EMAIL_HOST']
+# EMAIL_PORT = local_settings.Email_Configuration['EMAIL_PORT']
+# EMAIL_HOST_USER = local_settings.Email_Configuration['EMAIL_HOST_USER']
+# EMAIL_HOST_PASSWORD = local_settings.Email_Configuration['EMAIL_HOST_PASSWORD']
+# EMAIL_USE_TLS = local_settings.Email_Configuration['EMAIL_USE_TLS']
 
 GRAPHENE = {
   'SCHEMA': 'graphql_api.schema.schema'
