2a0125fa48f107309d206e0e168ef8cc46138c22
masih1999-12 <msh77742@gmail.com>
2024-03-27 20:18:19 +0330

    add some serializer and view and urls for dashboard_student app

--- back-end/dashboard_student/serializers.py
+++ back-end/dashboard_student/serializers.py
@@ -1,29 +1,11 @@
 from rest_framework import serializers
-from education.models import SemesterCourse , Semester , Department , Course , StudentCourse
+from education.models import SemesterClass, SemesterCourse , Semester , Department , Course , StudentCourse
 from accounts.models import Student, Teacher , User
 from .models import (
     EnrollmentRequest,
 )
 
-class DepartmentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Department
-        fields = ['department_name', 'department_code', 'year_established', 
-                  'department_location']
-        
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username','first_name' ,'last_name','email' , 'user_number', 
-                  'national_code', 'birthday', 'profile_image', 'phone', 'address', 'gender']
-
-class TeacherSerializer(serializers.ModelSerializer):
-    user = UserSerializer() 
-    department = DepartmentSerializer()
-    class Meta:
-        model = Teacher
-        fields = ['user', 'expertise', 'rank', 'department']
 class EnrollmentRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = EnrollmentRequest
@@ -39,45 +21,79 @@ class StudentEnrollmentRequestSerializer(serializers.ModelSerializer):
         model = EnrollmentRequest
         exclude = ['is_approved']
         
-class SemesterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Semester
-        fields = ['name','start_semester','end_semester','semester_type',]
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        prerequisite = serializers.PrimaryKeyRelatedField(many=True, queryset=Course.objects.all())
-        corequisite = serializers.PrimaryKeyRelatedField(many=True, queryset=Course.objects.all())
-        fields = ['course_name','course_code','credit_num','prerequisite','corequisite']
-class SemesterCourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SemesterCourse
-        semester = SemesterSerializer
-        instructor = TeacherSerializer
-        course = CourseSerializer
-        fields = ['semester','course','class_days','class_time','exam_datetime',
-                  'exam_location','instructor','course_capacity']
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username','first_name' ,'last_name','email' , 'user_number', 
-                  'national_code', 'birthday', 'profile_image', 'phone', 'address', 'gender']
-
+        
 class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Department
         fields = ['department_name', 'department_code', 'year_established', 
                   'department_location']
+        
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name' ,'last_name','profile_image']
+
+class TeacherSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    class Meta:
+        model = Teacher
+        fields = ['user']
+class SemesterClassSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SemesterClass
+        fields = ['classes_start', 'classes_end']
+class SemesterSerializer(serializers.ModelSerializer):
+    classes = SemesterClassSerializer()
+    class Meta:
+        model = Semester
+        fields = ['name' , 'classes']
+class CourseSerializer(serializers.ModelSerializer):
+    prerequisite = serializers.PrimaryKeyRelatedField(many=True, queryset=Course.objects.all())
+    corequisite = serializers.PrimaryKeyRelatedField(many=True, queryset=Course.objects.all())
+    class Meta:
+        model = Course
+        fields = ['course_name','course_code','credit_num','prerequisite','corequisite']
+class SemesterCourseSerializer(serializers.ModelSerializer):
+    semester = SemesterSerializer()
+    course = CourseSerializer()
+    instructor = TeacherSerializer()
+    class Meta:
+        model = SemesterCourse
+        fields = ['semester','course','class_days','class_time',
+                  'instructor','course_capacity','remain_course_capacity']
 class StudentSerializer(serializers.ModelSerializer):
     user = UserSerializer() 
+    advisor = TeacherSerializer()
     class Meta:
         model = Student
         fields = ['user', 'entry_semester', 'gpa', 'entry_year'
                   , 'advisor', 'military_service_status', 'year_of_study','major']
 class StudentCourseSerializer(serializers.ModelSerializer):
+    semester_course = SemesterCourseSerializer()
     class Meta:
         model = StudentCourse
-        student = StudentSerializer
-        semester_course = SemesterCourseSerializer
-        fields = ['student','semester_course','status','score']
+        fields = ['semester_course','status','score']
+
+
+class ExamSemesterCourseSerializer(serializers.ModelSerializer):
+    semester = SemesterSerializer()
+    course = CourseSerializer()
+    class Meta:
+        model = SemesterCourse
+        fields = ['semester','course','exam_datetime' , 'exam_location']
+
+class ExamStudentCourseSerializer(serializers.ModelSerializer):
+    semester_course = ExamSemesterCourseSerializer()
+    class Meta:
+        model = StudentCourse
+        fields = ['semester_course']
+
+
+class ProfileStudentSerializer(serializers.ModelSerializer):
+    user = UserSerializer() 
+    advisor = TeacherSerializer()
+    class Meta:
+        model = Student
+        fields = ['user','entry_semester', 'gpa', 'entry_year'
+                  , 'advisor', 'military_service_status', 'year_of_study','major']
