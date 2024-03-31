af2e026d7cabb50d5ddb97fbe4a526971d9c0341
masih1999-12 <msh77742@gmail.com>
2024-04-01 02:59:46 +0330

    add new views and serializers and urls in dashboard_student app

--- back-end/dashboard_student/serializers.py
+++ back-end/dashboard_student/serializers.py
@@ -6,6 +6,7 @@ from .models import SemesterRegistrationRequest , RevisionRequest , AddRemoveReq
                     EnrollmentRequest , EmergencyRemovalRequest , StudentDeleteSemesterRequest , \
                     EmploymentEducationRequest, UnitSelectionRequest
 from django.utils import timezone
+from rest_framework.exceptions import NotFound
 
 
 # class EnrollmentRequestSerializer(serializers.ModelSerializer):
@@ -116,9 +117,10 @@ class ExamStudentCourseSerializer(serializers.ModelSerializer):
         fields = ['semester_course']
 
 class MajorSerializer(serializers.ModelSerializer):
+    department = DepartmentSerializer()
     class Meta:
         model = Major
-        fields = ['major_name','major_code', 'level', 'education_group']
+        fields = ['major_name','major_code', 'level', 'education_group' , 'department']
 
 class ProfileStudentSerializer(serializers.ModelSerializer):
     user = UserSerializer() 
@@ -245,3 +247,159 @@ class UnitSelectionRequestSerializer(serializers.ModelSerializer):
                     SemesterCourseSerializer(courses,many = True).data}"
             
             raise serializers.ValidationError(message_wrong_semester+message_no_capacity)
+        
+class StudentDeleteSemesterRequestSerializer(serializers.ModelSerializer):
+    semester_registration_request = UnitSelectionSemesterRegistrationRequestSerializer()
+    class Meta:
+        model = StudentDeleteSemesterRequest
+        fields = ['id','semester_registration_request', 'teacher_approval_status',
+                    'educational_assistant_approval_status' , 'created_at',
+                    'student_explanations','educational_assistant_explanation']
+        
+        read_only_fields = ['id','semester_registration_request', 'teacher_approval_status',
+                            'educational_assistant_approval_status' , 'created_at',
+                            'educational_assistant_explanation']
+        
+
+    
+    def get_fields(self):
+        fields = super().get_fields()
+
+        if self.context.get('request') and self.context['request'].method == 'POST':
+            fields['semester_registration_request'] = serializers.PrimaryKeyRelatedField(
+                queryset=StudentDeleteSemesterRequest.objects.all())
+        return fields
+    def create(self, validated_data):
+        semester_registration_request = validated_data.pop('semester_registration_request')
+        user = self.context['user']
+        semester= None
+        try:
+            semester_registration_request = StudentDeleteSemesterRequest.objects.get(
+                pk=semester_registration_request.pk, 
+                student__user = user)
+        except StudentDeleteSemesterRequest.DoesNotExist:
+            raise serializers.ValidationError("Invalid SemesterRegistrationRequest")
+        
+        existing_request = StudentDeleteSemesterRequest.objects.filter(
+            semester_registration_request=semester_registration_request, 
+            semester_registration_request__student__user=user).exists()
+        if existing_request:
+            raise serializers.ValidationError("A unitselection request for this semester already exists")
+        
+        try:
+            student = Student.objects.get(user = user)
+        except Student.DoesNotExist:
+            raise serializers.ValidationError("Invalid student")
+        
+        
+        semester = StudentDeleteSemesterRequest.objects.get(
+            pk = semester_registration_request.pk).semester
+        current_date = timezone.now().date()
+        
+        if current_date < semester.start_semester or \
+            current_date > semester.end_semester:
+                raise serializers.ValidationError("Unvalide semester")
+        
+        student_delete_semester_request = StudentDeleteSemesterRequest.objects.create(
+                semester_registration_request = semester_registration_request,
+                student_explanations = validated_data.get('student_explanations')
+                )
+        return student_delete_semester_request
+
+
+class RevisionRequestSerializer(serializers.ModelSerializer):
+    course = StudentCourseSerializer()
+    class Meta:
+        model = RevisionRequest
+        fields = ['id', 'course' ,'approval_status',
+                    'created_at' , 'text', 'answer']
+        
+        read_only_fields = ['id','course' ,'approval_status',
+                    'created_at' , 'answer' ]
+    
+    def get_fields(self):
+        fields = super().get_fields()
+
+        if self.context.get('request') and self.context['request'].method == 'POST':
+            fields['course'] = serializers.IntegerField()
+            
+        return fields
+    
+    def create(self, validated_data):
+        student_course_pk = validated_data.get('course')
+        user = self.context['user']
+        try:
+            student = Student.objects.get(user = user)
+        except:
+            raise serializers.ValidationError("Invalid student")
+        
+        try:
+            course = StudentCourse.objects.get(pk = student_course_pk , student = student)
+        except Exception:
+            raise serializers.ValidationError("Invalid course")
+
+        existing_request = RevisionRequest.objects.filter(
+                course=course, student=student ,approval_status = 'P').first()
+        
+        if existing_request!=None:
+            raise serializers.ValidationError("A registration request for this semester already exists")
+
+        
+        validated_data['course'] = course
+        revision_request = RevisionRequest.objects.create(
+            student = student , course = course , text = validated_data['text'])
+        
+        return revision_request
+    
+class EmergencyRemovalRequestSerializer(serializers.ModelSerializer):
+    course = StudentCourseSerializer()
+    class Meta:
+        model = EmergencyRemovalRequest
+        fields = ['id', 'course' ,'approval_status','created_at' , 
+                    'student_explanation','educational_assistant_explanation']
+        
+        read_only_fields = ['id','approval_status','created_at' , 
+                            'educational_assistant_explanation']
+    def get_fields(self):
+        fields = super().get_fields()
+
+        if self.context.get('request') and self.context['request'].method == 'POST':
+            fields['course'] = serializers.IntegerField()
+            
+        return fields
+    
+    def create(self, validated_data):
+        student_course_pk = validated_data.get('course')
+        user = self.context['user']
+        try:
+            student = Student.objects.get(user = user)
+        except:
+            raise serializers.ValidationError("Invalid student")
+        
+        try:
+            course = StudentCourse.objects.get(pk = student_course_pk , student = student)
+        except Exception:
+            raise serializers.ValidationError("Invalid course")
+
+        current_date = timezone.now().date()
+        emergency_start = course.semester_course.semester.emergency.emergency_remove_start
+        emergency_end = course.semester_course.semester.emergency.emergency_remove_end
+        
+        if current_date<emergency_start:
+            raise serializers.ValidationError("Emergency remove not accesible")
+        elif current_date>emergency_end:
+            raise serializers.ValidationError("Emergency remove ended")
+        
+        existing_request = EmergencyRemovalRequest.objects.filter(
+                course=course, student=student ,approval_status = 'P').first()
+        
+        if existing_request!=None:
+            raise serializers.ValidationError("A registration request for this semester already exists")
+
+        
+        validated_data['course'] = course
+        emergency_removal_request = EmergencyRemovalRequest.objects.create(
+            student = student , course = course , 
+            student_explanation = validated_data['student_explanation'])
+        
+        return emergency_removal_request
