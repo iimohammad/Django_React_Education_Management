1e29e5ec9b4468bae896a4e5fdeb9fb42186d2af
peyman <shojaie.payman32@gmail.com>
2024-04-02 15:31:39 +0330

    three change in merge

--- back-end/dashboard_educationalassistant/views.py
+++ back-end/dashboard_educationalassistant/views.py
@@ -1,24 +1,10 @@
 from django_filters.rest_framework import DjangoFilterBackend
-<<<<<<< HEAD
-from rest_framework import generics, viewsets
-=======
 from rest_framework import generics, viewsets, status, views
->>>>>>> main
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from accounts.models import Student, Teacher, EducationalAssistant
-<<<<<<< HEAD
-from education.models import StudentCourse, SemesterCourse
-from .filters import StudentFilter, TeacherFilter
-from .pagination import DefaultPagination
-from accounts.permissions import IsEducationalAssistant
-from .serializers import StudentSerializer, TeacherSerializer, EducationalAssistantSerializer, StudentCourseSerializer, SemesterCourseSerializer
-from django.db.models import Q
-from django.core.exceptions import ValidationError
-from rest_framework.decorators import action
-=======
 from education.models import Course, SemesterCourse, Major
 from dashboard_student.models import EmergencyRemovalRequest, StudentDeleteSemesterRequest, EmploymentEducationRequest
 
@@ -36,7 +22,6 @@ from .permissions import IsEducationalAssistant
 from .serializers import StudentSerializer, TeacherSerializer, EducationalAssistantSerializer, \
     StudentCoursePassSerializer
 from accounts.serializers import UserProfileImageUpdateSerializer
->>>>>>> main
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import Http404
@@ -112,126 +97,6 @@ class EducationalAssistantChangeProfileView(generics.RetrieveUpdateAPIView):
     permission_classes = [IsAuthenticated, IsEducationalAssistant]
 
     def get_object(self):
-<<<<<<< HEAD
-        return EducationalAssistant.objects.filter(user = self.request.user)
-
-
-class StudentCoursesInProgress(generics.ListAPIView):
-    serializer_class = StudentCourseSerializer
-    permission_classes = [IsAuthenticated,]
-    def get_queryset(self):
-        student_id = self.kwargs['student_id']
-        user = self.request.user
-        if int(student_id) == user.student.id:            
-            return StudentCourse.objects.filter(Q(student=student_id), Q(status='R'), Q(score__isnull=True) | Q(score__exact=''))
-
-        elif hasattr(user, 'teacher'):
-            students_with_advisor = Student.objects.filter(advisor=user.teacher)
-            try:
-                student_with_specific_id = students_with_advisor.get(id=student_id)
-            except Student.DoesNotExist:
-                raise ValidationError()
-            return StudentCourse.objects.filter(Q(student=student_id), Q(status='R'), Q(score__isnull=True) | Q(score__exact=''))
-        
-        elif hasattr(user, 'educationalassistant') :
-            student = Student.objects.get(id=student_id)  
-            department_of_student = student.major.department
-            if department_of_student == user.educationalassistant.field.department:
-                return StudentCourse.objects.filter(Q(student=student_id), Q(status='R'), Q(score__isnull=True) | Q(score__exact=''))
-            else:
-                raise ValidationError()
-        
-        elif hasattr(user, 'adminuser'):
-            return StudentCourse.objects.filter(Q(student=student_id), Q(status='R'), Q(score__isnull=True) | Q(score__exact=''))
-        
-        else:
-                raise ValidationError()
-
-
-class AcceptedStudentCourses(generics.ListAPIView):
-    serializer_class = StudentCourseSerializer
-    permission_classes = [IsAuthenticated,]
-
-    def get_queryset(self):
-        student_id = self.kwargs['student_id']
-        user = self.request.user
-        if int(student_id) == user.student.id:            
-            return StudentCourse.objects.filter(student=user.student, is_pass=True)        
-
-        elif hasattr(user, 'teacher'):
-            students_with_advisor = Student.objects.filter(advisor=user.teacher)
-            try:
-                student_with_specific_id = students_with_advisor.get(id=student_id)
-            except Student.DoesNotExist:
-                raise ValidationError("You don't have access.")
-            return StudentCourse.objects.filter(student=user.student, is_pass=True)        
-        
-        elif hasattr(user, 'educationalassistant') :
-            student = Student.objects.get(id=student_id)  
-            department_of_student = student.major.department
-            if department_of_student == user.educationalassistant.field.department:
-                return StudentCourse.objects.filter(student=user.student, is_pass=True)        
-            else:
-                raise ValidationError("You don't have access.")
-        
-        elif hasattr(user, 'adminuser'):
-            return StudentCourse.objects.filter(student=user.student, is_pass=True)        
-        else:
-                raise ValidationError("You don't have access.")
-
-
-
-
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from rest_framework import status
-
-class EducationAssistantApprovedViewSet(viewsets.ViewSet):
-    permission_classes = [IsAuthenticated, IsEducationalAssistant]
-    serializer_class = SemesterCourseSerializer
-    
-    def list(self, request):
-        educationalassistant = self.request.user.educationalassistant
-        queryset = SemesterCourse.objects.filter(instructor__department=educationalassistant)
-        serializer = self.serializer_class(queryset, many=True)
-        return Response(serializer.data)
-    
-    @action(detail=True, methods=['post'], name='Evaluate Students')
-    def evaluate_students(self, request, pk=None):
-        semester_course = self.get_semester_course(pk)
-        data = request.data.get('student_scores', [])
-
-        for entry in data:
-            student_id = entry.get('student_id')
-            score = entry.get('score')
-            action = entry.get('action')
-
-            # Fetch the student course instance
-            student_course = StudentCourse.objects.filter(
-                semester_course=semester_course, student_id=student_id).first()
-
-            if action in ['add', 'change']:
-                if student_course:
-                    # Update score if student course exists
-                    student_course.score = score
-                    student_course.save()
-
-        return Response({'message': 'Student scores updated successfully'}, status=status.HTTP_200_OK)
-
-    @action(detail=True, methods=['get'], name='Show Score Students')
-    def score_students(self, request, pk=None):
-        semester_course = self.get_semester_course(pk)
-        students = StudentCourse.objects.filter(semester_course=semester_course)
-        serializer = StudentCourseSerializer(students, many=True)
-        return Response(serializer.data)
-    
-    def get_semester_course(self, pk):
-        educationalassistant = self.request.user.educationalassistant
-        try:
-            return SemesterCourse.objects.get(id=pk, instructor__department=educationalassistant)
-        except SemesterCourse.DoesNotExist:
-            raise Http404
-=======
         return EducationalAssistant.objects.filter(user=self.request.user)
 
 
@@ -444,4 +309,3 @@ class EmploymentEducationRequestViewSet(viewsets.ModelViewSet):
         Disallow DELETE requests.
         """
         return Response({"detail": "DELETE requests are not allowed."}, status=405)
->>>>>>> main
