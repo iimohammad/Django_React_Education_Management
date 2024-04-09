aad02f2e6cfbf5c3d200e29bc1050a799c5c8b85
masih1999-12 <msh77742@gmail.com>
2024-04-09 23:34:57 +0330

    modify dashboard_professors permissions

--- back-end/dashboard_professors/permissions.py
+++ back-end/dashboard_professors/permissions.py
@@ -1,22 +1,15 @@
 from rest_framework.permissions import BasePermission
-
-class IsEducationalAssistant(BasePermission):
-    def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            return False
-
-        return hasattr(request.user, 'educationalassistant')
-
-
-class IsStudent(BasePermission):
-    def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            return False
-
-        return hasattr(request.user, 'student')
+from accounts.models import Teacher
 class IsTeacher(BasePermission):
     def has_permission(self, request, view):
         if not request.user.is_authenticated:
             return False
 
         return hasattr(request.user, 'teacher')
+    
+class IsAdvisor(BasePermission):
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+        return Teacher.objects.get(user = request.user).can_be_advisor
+        
