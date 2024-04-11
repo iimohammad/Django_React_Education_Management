f26f7e4278d79de894130c9ee7c77cea0429fb0c
Amirabbas <abb.afrasiyabi@gmail.com>
2024-04-11 23:15:12 +0330

    update test.py

--- back-end/dashboard_student/tests.py
+++ back-end/dashboard_student/tests.py
@@ -4,16 +4,10 @@ from accounts.models import Student, Teacher
 from education.models import Department, Major
 
 User = get_user_model()
-
 class SemesterRegistrationRequestTestCase(TestCase):
     def setUp(self):
-        # Create a user
         self.user = User.objects.create_user(username='testteacher', password='testpassword')
-        
-        # Create a department
         self.department = Department.objects.create(department_name='Test Department', department_code=1, year_established='2022-01-01', department_location='Test Location')
-
-        # Create a major
         self.major = Major.objects.create(
             major_name='Computer Science',
             major_code=1,
@@ -21,9 +15,7 @@ class SemesterRegistrationRequestTestCase(TestCase):
             number_of_credits=120,
             education_group='Science'
         )
-
     def test_student_creation_with_advisor(self):
-        # Create a teacher
         self.teacher = Teacher.objects.create(
             user=self.user,
             expertise='Test Expertise',
@@ -31,21 +23,16 @@ class SemesterRegistrationRequestTestCase(TestCase):
             department=self.department,
             can_be_advisor=True
         )
-        
-        # Create a student with the teacher as advisor
         self.student = Student.objects.create(
             user=self.user,
             entry_semester='Spring',
             entry_year=2022,
-            advisor=self.teacher,  # Set the advisor field
+            advisor=self.teacher, 
             year_of_study=1,
             major=self.major
         )
-        
         saved_student = Student.objects.get(user=self.user)
-        
-        # Check if the student is created successfully
         self.assertEqual(saved_student.entry_semester, 'Spring')
-        self.assertEqual(int(saved_student.entry_year), 2022)  # Convert to integer before comparison
+        self.assertEqual(int(saved_student.entry_year), 2022)  
         self.assertEqual(saved_student.advisor, self.teacher)
         self.assertEqual(saved_student.year_of_study, 1)
