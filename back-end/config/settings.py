32debd818d8079287fcba0e16d7da9ededfc92be
Mohammad Baharlou <mohammadbaharloo96@gmail.com>
2024-04-13 02:40:01 +0330

    admin settings

--- back-end/config/settings.py
+++ back-end/config/settings.py
@@ -292,3 +292,41 @@ LOGGING = {
 MINIO_ENDPOINT = 'http://127.0.0.1:9000'
 MINIO_ACCESS_KEY = 'ZTG1e9zxdppm3Yc2y5lH'
 MINIO_SECRET_KEY = 'dxLFJQpU2un2y0rWwGfSle2qS4HKoSQKnmOeKZLj'
+
+JAZZMIN_SETTINGS = {
+    'site_title' : "Education Managment",
+    'site_headers' : "Education Managment",
+    'site_brand' : "Education Managment",
+    'show_ui_builder' : True,
+}
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": True,
+    "brand_small_text": False,
+    "brand_colour": "navbar-teal",
+    "accent": "accent-orange",
+    "navbar": "navbar-lightblue navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-dark-orange",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "simplex",
+    "dark_mode_theme": "darkly",
+    "button_classes": {
+        "primary": "btn-outline-primary",
+        "secondary": "btn-outline-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    }
+}
